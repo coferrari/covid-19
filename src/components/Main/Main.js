5e13f744fe8d7cf49d2e61644094f1107328e60d
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-11-08 15:31:42 -0300

    Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getAll } from "../../redux/actions/index";
+import { useDispatch, useSelector } from "react-redux";
 
 const Main = () => {
+    const dispatch = useDispatch()
+    const { statistics } = useSelector((state) => state.statistics)
+
+    useEffect(() => {
+        dispatch(getAll())
+    }, [dispatch])
+
     return (
         <div>
-            main
+            {statistics?.map((country) => <h5>{country.country}</h5>)}
+
         </div>
     )
 }
