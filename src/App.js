29c5c5103eead80b5b6a84201704a2d69ea5f1d0
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-11-08 18:19:40 -0300

    route detail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
+import Details from "./components/Details/Details";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Nav />
-        <Main/>
         <Routes>
-          <Route path="/" component={Main}>
-          </Route>
+          <Route exact path="/" element={<Main />} />
+          <Route exact path="country/:country" element={<Details />} />
         </Routes>
       </BrowserRouter>
     </div>
