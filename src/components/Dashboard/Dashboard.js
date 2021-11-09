fd9306908f0d14138a87e6c95e874b753471c58d
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-11-09 12:18:43 -0300

    loader added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { formatNumber } from "../../utils";
+import style from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  const { statistics } = useSelector((state) => state.statistics);
+  const { statistics, requestAPI } = useSelector((state) => state.statistics);
 
   const countCountries = statistics?.reduce((acc, curr) => {
     return curr.cases.new ? acc + 1 : acc + 0;
@@ -31,17 +33,38 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h1>COVID-19</h1>
-      <div>
-        <h5>Countries with confirmed cases: {countCountries}</h5>
-      </div>
-      <div>
-        <h5>Total cases: {formatNumber(totalCases)}</h5>
-        <h6>New cases: {formatNumber(newCases)}</h6>
-      </div>
-      <div>
-        <h5>Total deaths: {formatNumber(totalDeaths)}</h5>
-        <h6>New deaths: {formatNumber(newDeaths)}</h6>
+      <Link to="/">
+        <h1>COVID-19</h1>
+      </Link>
+      <div>global information</div>
+      <div className={style.grid}>
+        <div className={style.info}>
+          {!requestAPI ? (
+            <h5>Countries with confirmed cases: {countCountries}</h5>
+          ) : (
+            <div className={style.loader}></div>
+          )}
+        </div>
+        <div className={style.info}>
+          {!requestAPI ? (
+            <div>
+              <h5>Total cases: {formatNumber(totalCases)}</h5>
+              <h6>New cases: {formatNumber(newCases)}</h6>
+            </div>
+          ) : (
+            <div className={style.loader}></div>
+          )}
+        </div>
+        <div className={style.info}>
+          {!requestAPI ? (
+            <div>
+              <h5>Total deaths: {formatNumber(totalDeaths)}</h5>
+              <h6>New deaths: {formatNumber(newDeaths)}</h6>
+            </div>
+          ) : (
+            <div className={style.loader}></div>
+          )}
+        </div>
       </div>
     </div>
   );
