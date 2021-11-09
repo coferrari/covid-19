79837b0625b9decc6276771c1acdfab68ca12555
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-11-09 12:20:20 -0300

    clear added, dynamic search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,29 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { searchCountry, filterCountries } from "../../redux/actions";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  filterCountries,
+  setInputSearch,
+  clearInputSearch,
+} from "../../redux/actions";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
-  const [input, setInput] = useState("a");
   const dispatch = useDispatch();
+  const { searchInput } = useSelector((state) => state.search);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   dispatch(filterCountries(input));
-  // }, [input]);
+  useEffect(() => {
+    dispatch(filterCountries(searchInput));
+  }, [dispatch, searchInput]);
 
   const handleChange = (e) => {
     e.preventDefault();
-    setInput(e.target.value);
+    dispatch(setInputSearch(e.target.value));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(searchCountry(input));
+    navigate("/");
+  };
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    dispatch(clearInputSearch());
   };
 
   return (
     <div>
-      <input onChange={(e) => handleChange(e)}></input>
-      <button onClick={(e) => handleSubmit(e)}>Search</button>
+      <input
+        onChange={(e) => handleChange(e)}
+        value={searchInput}
+        placeholder="Search..."
+      ></input>
+      {pathname !== "/" ? (
+        <button onClick={(e) => handleSubmit(e)}>Search</button>
+      ) : (
+        <button onClick={(e) => handleClear(e)}>Clear filter</button>
+      )}
     </div>
   );
 };
