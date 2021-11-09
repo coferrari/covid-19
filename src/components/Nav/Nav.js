46ca776cac73260860ff90f9a3649e320df7d52b
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-11-09 12:22:26 -0300

    Nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "../Dashboard/Dashboard";
+import SearchBar from "../SearchBar/SearchBar";
+
+const Nav = () => {
+  return (
+    <div className="container">
+      <Dashboard />
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Nav;
