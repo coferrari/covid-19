e1dee3e6b1602870c32779e13b0ae62150489ccd
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-11-09 12:21:50 -0300

    sorts added

--- src/redux/reducer/statisticsReducer.js
+++ src/redux/reducer/statisticsReducer.js
@@ -1,44 +1,63 @@
-import { GET_ALL, COUNTRY_ORDER, COUNTRY_FILTER } from "../actions/types";
+import { 
+  COUNTRY_FILTER,   
+  GET_ALL_REQUEST,
+  GET_ALL_SUCCESS,
+  GET_ALL_FAILURE, 
+} from "../actions/types";
 
 const initialState = {
+  loading: true,
+  requestAPI: false,
+  error: {},
   statistics: [],
-  orderedStatistics: [],
-  filteredStatistics: []
+  filteredStatistics: [],
 };
 
 const statisticsReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case GET_ALL:
+    case GET_ALL_REQUEST: 
+    return {
+      ...state,
+      requestAPI: true
+    }
+    case GET_ALL_SUCCESS:
+      const countriesOrdered = payload.sort((a, b) => {
+        if (a.country > b.country) return 1;
+        else {
+          return -1;
+        }
+      });
+      const all = countriesOrdered.filter((country) => country.continent === "All")
+      const africa = countriesOrdered.filter((country) => country.continent === "Africa")
+      const asia = countriesOrdered.filter((country) => country.continent === "Asia")
+      const europe = countriesOrdered.filter((country) => country.continent === "Europe")
+      const oceania = countriesOrdered.filter((country) => country.continent === "Oceania")
+      const northAmerica = countriesOrdered.filter((country) => country.continent === "North-America")
+      const southAmerica = countriesOrdered.filter((country) => country.continent === "South-America")
+      const rest = countriesOrdered.filter((country) => !country.continent)
       return {
         ...state,
-        statistics: payload,
-        filteredStatistics: payload
+        loading: false,
+        requestAPI: false,
+        statistics: [...all, ...africa, ...asia, ...europe, ...oceania, ...northAmerica, ...southAmerica, ...rest],
+        filteredStatistics: [...all, ...africa, ...asia, ...europe, ...oceania, ...northAmerica, ...southAmerica, ...rest],
       };
+    case GET_ALL_FAILURE: 
+      return {
+        ...state,
+        error: payload
+      }
     case COUNTRY_FILTER:
       const countries = state.statistics;
-      const countriesFiltered = payload ? countries.filter((country) => country.name.toLowerCase().includes(payload.toLowerCase())) : countries
+      const countriesFiltered = payload
+        ? countries.filter((country) =>
+            country.country.toLowerCase().includes(payload.toLowerCase())
+          )
+        : countries;
       return {
         ...state,
         statistics: countries,
-        filteredStatistics: countriesFiltered
-      }
-    case COUNTRY_ORDER:
-      let ordered;
-      if (payload === true) {
-        ordered = state.statistics.sort((a, b) => {
-          if (a.country > b.country) return 1;
-          else return -1;
-        });
-      } 
-      if (payload === false) {
-        ordered = state.statistics.sort((a, b) => {
-          if (a.country < b.country) return 1;
-          else return -1;
-        });
-      }
-      return {
-        ...state,
-        orderedStatistics: ordered,
+        filteredStatistics: countriesFiltered,
       };
     default:
       return state;
