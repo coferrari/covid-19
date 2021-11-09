ef0a5cb5dbce00e14ab85177946049eea0793616
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-11-09 12:20:43 -0300

    loader added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,41 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import { Table } from "react-bootstrap";
 import Country from "../Country/Country";
-import { countryOrder } from "../../redux/actions/index";
 
-const Statistics = ({ statistics }) => {
-  const [az, setAz] = useState(false);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(countryOrder(az))
-  }, [dispatch, az])
-
-  const handleOrderCountry = (e) => {
-    e.preventDefault()
-    setAz(!az);
-  }
-
+const Statistics = ({ statistics, loading, requestAPI }) => {
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th onClick={handleOrderCountry}>Country</th>
-          <th>Continent</th>
-          <th>Population</th>
-          <th>Cases</th>
-          <th>Tests</th>
-          <th>Deaths</th>
-          <th>Last update</th>
-        </tr>
-      </thead>
-      <tbody>
-        {statistics?.map((country, i) => (
-          <Country key={i} country={country} />
-        ))}
-      </tbody>
-    </Table>
+    <div className="container">
+      {loading && requestAPI && <div>loading updated COVID-19 data...</div>}
+
+      {!loading && statistics && (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Country</th>
+              <th>Continent</th>
+              <th>Population</th>
+              <th>Cases</th>
+              <th>Tests</th>
+              <th>Deaths</th>
+              <th>Last update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {statistics?.map((country, i) => (
+              <Country key={i} country={country} />
+            ))}
+          </tbody>
+        </Table>
+      )}
+      {!statistics.length && !requestAPI && <div>no matches</div>}
+    </div>
   );
 };
 
